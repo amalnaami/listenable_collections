fa7cac791040cd5e05cbec13279b4e7422ef6235
shatha.elnaami <75214477+amalnaami@users.noreply.github.com>
2024-06-06 11:39:04 +0300

    remove unnecessary comments

--- test/list_notifier_test.dart
+++ test/list_notifier_test.dart
@@ -24,21 +24,7 @@ void main() {
 
       list.swap(0, 2);
 
-      /// Swap the values at these indexes
       expect(result, [3, 2, 1]);
-    });
-
-    test("list length and index's value", () {
-      buildListener();
-
-      final length = list.length;
-      final firstItem = list[0];
-
-      /// To get the length of the list
-      expect(length, 3);
-
-      /// To get value at index
-      expect(firstItem, 1);
     });
 
     test("Listeners get updated if a value gets added", () {
@@ -46,7 +32,6 @@ void main() {
 
       list.add(4);
 
-      /// Add value 4 to the end of the list
       expect(result[3], 4);
     });
 
@@ -55,7 +40,6 @@ void main() {
 
       list.addAll([4, 5]);
 
-      /// Add values 4 and 5 to the end of the list
       expect(result, [1, 2, 3, 4, 5]);
     });
 
@@ -64,7 +48,6 @@ void main() {
 
       list.clear();
 
-      /// Clear the list
       expect(result, []);
     });
 
@@ -73,7 +56,6 @@ void main() {
 
       list.fillRange(0, list.length, 1);
 
-      /// Fill the list with value(1) from index 0 to the end
       expect(result, [1, 1, 1]);
     });
 
@@ -82,7 +64,6 @@ void main() {
 
       list.insert(1, 1);
 
-      /// Insert value 1 at index 1 then complete the list
       expect(result, [1, 1, 2, 3]);
     });
 
@@ -91,7 +72,6 @@ void main() {
 
       list.insertAll(1, [1, 2]);
 
-      /// Insert values 1 and 2 at index 1 then complete the list
       expect(result, [1, 1, 2, 2, 3]);
     });
 
@@ -100,10 +80,7 @@ void main() {
 
       final itemIsRemoved = list.remove(2);
 
-      /// remove(value): mean remove value 2 from the list
       expect(result, [1, 3]);
-
-      /// Check if removed value is equal to 2
       expect(itemIsRemoved, true);
     });
 
@@ -112,10 +89,7 @@ void main() {
 
       final removedItem = list.removeAt(1);
 
-      /// removeAt(index): mean remove value in the index 1 from the list
       expect(result, [1, 3]);
-
-      /// Removed value at index 1 is equal to 2
       expect(removedItem, 2);
     });
 
@@ -124,10 +98,7 @@ void main() {
 
       final itemRemoved = list.removeLast();
 
-      /// Remove the last value from the list
       expect(result, [1, 2]);
-
-      /// Removed value is equal to 3
       expect(itemRemoved, 3);
     });
 
@@ -136,7 +107,6 @@ void main() {
 
       list.removeRange(0, 2);
 
-      /// Remove values from index 0 to before index 2
       expect(result, [3]);
     });
 
@@ -145,7 +115,6 @@ void main() {
 
       list.removeWhere((element) => element == 1);
 
-      /// Remove all elements that are equal to 1
       expect(list.value, [2, 3]);
     });
 
@@ -153,9 +122,7 @@ void main() {
       buildListener();
 
       list.replaceRange(0, 2, [3, 3]);
-      /// Replace values from index 0 to before index 2 with [3, 3]
-      /// if the replacement list is [3] the result will be [3, 3]
-      /// if the replacement list is [3, 3, 3] the result will be [3, 3, 3, 3]
+
       expect(result, [3, 3, 3]);
     });
 
@@ -164,7 +131,6 @@ void main() {
 
       list.retainWhere((element) => element == 1);
 
-      /// Retain all elements that verify the condition
       expect(list.value, [1]);
     });
 
@@ -173,10 +139,6 @@ void main() {
 
       list.setAll(2, [2]);
 
-      /// Set all values from index 2 to the end with values in the newList
-      /// index 2 is 3, so it will be replaced with 2
-      /// if the list was [1, 2, 3] and we setAll(0, [5, 6]) it would be [5, 6, 3]
-      /// if the list was [1, 2, 3] and we setAll(2, [5, 6]) it would be [1, 2, 5, 6]
       expect(list.value, [1, 2, 2]);
     });
 
@@ -185,12 +147,6 @@ void main() {
 
       list.setRange(2, list.length, [2]);
 
-      /// Set values from index 2 to the end with values in the replacement list
-      /// index 2 is 3, so it will be replaced with 2
-      /// if setRange(0, 2, [5, 6]) it would be [5, 6, 3]
-      /// [5,6] will replace [1,2] in the list [5, 6, 3]
-      /// the replacement list should contain num of elements equal to the range
-      /// if setRange(0, 2, [5, 6, 7]) it would be [5, 6, 7] as the replacement list
       expect(result, [1, 2, 2]);
     });
 
@@ -199,7 +155,6 @@ void main() {
 
       list.shuffle();
 
-      /// Shuffle the list
       expect(result != [1, 2, 3], true);
     });
 
@@ -208,8 +163,6 @@ void main() {
 
       list.sort((value1, value2) => -(value1.compareTo(value2)));
 
-      /// Sort the list in descending(-) order
-      /// Sort the list regarding the condition
       expect(result, [3, 2, 1]);
     });
   });
